7909dadd76c289f4a9c2f7f7c92881d2434b493f
Ishaque <63145331+isaq12@users.noreply.github.com>
2024-01-28 03:05:19 -0600

    V1.0

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './**/*.html',
+    // Include other file types if you have Tailwind classes in your JS files, etc.
+    // './**/*.js',
+  ],
+  theme: {
+    extend: {scrollBehavior: ['smooth'],},
+  },
+  plugins: [],
+}
